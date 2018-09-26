6ff31251bc554d85322091b7529a504c56a7fc2f
Rok Roskar <roskarr@ethz.ch>
2018-09-27 00:41:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# renku-jupyterlab
+
+Nothing lasts forever. Except for reproducible research made with Renku.
+
+
+## Prerequisites
+
+* JupyterLab
+
+## Installation
+
+```bash
+jupyter labextension install renku-jupyterlab
+```
+
+## Development
+
+For a development install (requires npm version 4 or later), do the following in the repository directory:
+
+```bash
+npm install
+npm run build
+jupyter labextension link .
+```
+
+To rebuild the package and the JupyterLab app:
+
+```bash
+npm run build
+jupyter lab build
+```
+
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import {
+  JupyterLab, JupyterLabPlugin
+} from '@jupyterlab/application';
+
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
+
+import '../style/index.css';
+
+
+/**
+ * Initialization data for the renku-jupyterlab extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'renku-jupyterlab',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+    console.log('JupyterLab extension renku-jupyterlab is activated!');
+    console.log('ICommandPalette:', palette);
+
+  // Create a single widget
+  let widget: Widget = new Widget();
+  widget.id = 'renku-run';
+  widget.title.label = 'renku run';
+  widget.title.closable = true;
+
+    // Add an application command
+  const command: string = 'renku:run';
+  app.commands.addCommand(command, {
+    label: 'Run notebook with renku',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.addToMainArea(widget);
+      }
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({command, category: 'Renku'});
+
+  }
+};
+
+export default extension;
